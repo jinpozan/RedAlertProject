8918b955fb64fe2222b1409262068f23da6352d2
jinpozan <jinpozan@hotmail.com>
2018-05-11 20:01:37 +0800

    summer
    create

---
+++ Assets/DesignModeCode/DM03Strategy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DM03Strategy: MonoBehaviour {
+
+    void Start()
+    {
+        StrategyContext context = new StrategyContext();
+        context.strategy = new ConcreteStrategyA();
+        context.Cal();
+
+        context.strategy = new ConcreteStrategyB();
+        context.Cal();
+    }
+
+}
+
+public class StrategyContext
+{
+    public IStrategy strategy;
+    public void Cal() { strategy.Cal(); }
+}
+
+public interface IStrategy
+{
+    void Cal();
+}
+
+public class ConcreteStrategyA:IStrategy
+{
+    public void Cal()
+    {
+        Debug.Log("use strategy A ...");
+    }
+}
+
+public class ConcreteStrategyB : IStrategy
+{
+    public void Cal()
+    {
+        Debug.Log("use strategy B ...");
+    }
+}
