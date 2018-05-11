5187c4e4f721f5de43107af6a14f25d432b21a77
陈伟 <chenwei@chenweideMacBook-Pro.local>
2018-05-12 00:10:56 +0800

    add attr

--- Assets/Scripts/CharacterSystem/Attr/ICharacterAttr.cs
+++ Assets/Scripts/CharacterSystem/Attr/ICharacterAttr.cs
@@ -9,4 +9,9 @@ public abstract class ICharacterAttr {
     protected float mMoveSpeed;
     protected int mCurrentHP;
     protected string mIconSprite;
+
+	protected int mLv;
+	protected float mCritRate; // 0 - 1  暴击率
+
+   //增加的最大血量 抵御的伤害值 暴击增加的伤害
 }
